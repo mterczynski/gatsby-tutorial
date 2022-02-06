8b1757fcb192d0a65179d331ef2e03acbdc7ed48
Michał Terczyński <mterczynski271@gmail.com>
2022-02-06 20:54:28 +0100

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,43 +1,36 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { graphql, Link } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
-  {
+  query {
     allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        parent {
-          ... on File {
-            modifiedTime(formatString: "MMMM D, YYYY")
-          }
-        }
         frontmatter {
-          title
           date(formatString: "MMMM D, YYYY")
+          title
         }
         id
-        body
+        slug
       }
     }
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
